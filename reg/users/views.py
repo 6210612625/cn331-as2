0fca94ef3123d8a73b028e460d3104f2c3baefa8
anuradee <6210612625@student.tu.ac.th>
2021-09-16 13:08:50 +0000

    first commit users

---
+++ reg/users/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("users:login"))
+    
+    return render(request, "users/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username = username, password = password)
+        
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("user:index"))
+        
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid credential."
+            })
+    
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    pass
