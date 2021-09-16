b372829509f98b1a719a81770489603ea1ba309c
anuradee <6210612625@student.tu.ac.th>
2021-09-16 13:13:06 +0000

    added users

--- reg/reg/urls.py
+++ reg/reg/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
     path('subject/', include('subject.urls')),
     path('user/', include('user.urls')),
+    path('users/', include('users.urls')),
+
 ]
