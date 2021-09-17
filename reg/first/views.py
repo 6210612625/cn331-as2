dcf6acd5dd01d3b26ca84a8f3de18270a17f66e7
Nitcharee Khatkhanangchanan <6210612617@student.tu.ac.th>
2021-09-17 14:36:15 +0000

    committedddd

--- reg/first/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse("<h1>Hi</h1>")
-def greet(request, name):
-    return HttpResponse(f"<h1>reg,{name.title()}")
