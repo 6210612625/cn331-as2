0269917ba29cd083571844a77ac46d08f1213abf
anuradee <6210612625@student.tu.ac.th>
2021-09-17 14:25:44 +0000

    second commit users

--- reg/reg/settings.py
+++ reg/reg/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'first',
     'subject',
-    'user'
+    'users',
 ]
 
 MIDDLEWARE = [
--- reg/users/views.py
+++ reg/users/views.py
@@ -8,25 +8,27 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("users:login"))
-    
+
     return render(request, "users/index.html")
 
 def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(username = username, password = password)
-        
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("user:index"))
-        
+        users = authenticate(username=username,password=password)
+        if users is not None:
+            login(request, users)
+            return HttpResponseRedirect(reverse("users:index"))
         else:
-            return render(request, "users/login.html", {
-                "message": "Invalid credential."
+            return render(request, "users/login.html",{
+                "message": "Invalid Credential."
             })
-    
+
     return render(request, "users/login.html")
 
 def logout_view(request):
-    pass
+    logout(request)
+
+    return render(request, "users/login.html", {
+        "message": "Logged out."
+    })
