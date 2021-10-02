b09641f2b9e52a3d23a52cc4803e0098d44cce63
Nitcharee Khatkhanangchanan <6210612617@student.tu.ac.th>
2021-10-02 22:14:17 +0000

    final commit

--- regweb/courses/tests.py
+++ regweb/courses/tests.py
@@ -11,7 +11,7 @@ from django.shortcuts import get_list_or_404, get_object_or_404
 class CourseTestCase(TestCase):
     def setUp(self):
         course1 = Course.objects.create(code = "JA111" , name  = "A", semester =1,year = 2020 ,quota = 2,status = True)
-
+        course2 = Course.objects.create(code = "JA222" , name  = "B", semester =1,year = 2020 ,quota = 2,status = False)
 
     def test_seat_available(self):
         """ is_seat_available should be True """
@@ -39,13 +39,42 @@ class CourseTestCase(TestCase):
         
     def test_course(self):
         c = Client()
-        response = c.get(reverse('courses:courses'))
+        course = Course.objects.first()
+        response = c.get(reverse('courses:course',args=(course.id,)))
         self.assertEqual(response.status_code, 200)
-        try:
-            obj = Course.objects.get(pk=1)
-        except MyModel.DoesNotExist:
-            raise Http404("No MyModel matches the given query.")
+    
+    def test_book_successful(self):
+        c = Client()
+        user3 = User.objects.create(username="user3", password="", email="user3@example.com")
+        course = Course.objects.first()
+        c.force_login(user3)
+        response = c.get(reverse('courses:book',args=(course.id,)))        
+        self.assertEqual(course.student.count(),1)
         
+    def test_book_unsuccessful(self):
+        c = Client()
+        course = Course.objects.first()
+        response = c.get(reverse('courses:book',args=(course.id,)))        
+        self.assertEqual(course.student.count(),0)
+        
+    def test_remove_successful(self):
+        c = Client()
+        user3 = User.objects.create(username="user3", password="", email="user3@example.com")
+        course = Course.objects.first()
+        c.force_login(user3)
+        course.student.add(user3)
+        response = c.get(reverse('courses:remove',args=(course.id,)))        
+        self.assertEqual(course.student.count(),0)
+        
+    def test_remove_unsuccessful(self):
+        c = Client()
+        course = Course.objects.first()
+        response = c.get(reverse('courses:remove',args=(course.id,)))        
+        self.assertEqual(course.student.count(),0)
+         
+
+    
+
 
     
 
--- regweb/users/tests.py
+++ regweb/users/tests.py
@@ -2,13 +2,17 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+
 
 
 # Create your tests here.
 
 class UserViewTestCase(TestCase):
     def setUp(self):
-        User.objects.create(username='user1', password='', email='user1@example.com')
+        password = make_password('1234')
+        User.objects.create(username='user1', password=password, email='user1@example.com')
 
     def test_index_view_with_authenciation(self):
         c = Client()
@@ -22,9 +26,38 @@ class UserViewTestCase(TestCase):
         user = User.objects.get(username='user1')
         response = c.get(reverse('users:index'))
         self.assertEqual(response.status_code, 302)
-        
+
     def test_login_view_unsuccessful(self):
         c = Client()
         user = User.objects.get(username='user1')
-        response = c.post(reverse('users:login'), {'username': 'user1', 'password': ''})        
+        response = c.post(reverse('users:login'), {'username': 'user1', 'password': '1264'})
         self.assertEqual(response.status_code, 200)
+
+    def test_login_view_successful(self):
+        c = Client()
+        user = User.objects.get(username = "user1")
+        response = c.post(reverse('users:login') , {'username': 'user1' , 'password': '1234'})
+        self.assertEqual(response.status_code, 302 )
+
+    def test_login_view_index(self):
+        c = Client()
+        response = c.get(reverse('users:login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_view(self):
+        c = Client()
+        response = c.get(reverse('users:logout'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_studentcourse_without_authenciation(self):
+        c = Client()
+        response = c.get(reverse('users:studentcourse'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_studentcourse_with_authenciation(self):
+        c = Client()
+        user = User.objects.get(username='user1')
+        c.force_login(user)
+        response = c.get(reverse('users:studentcourse'))
+        self.assertEqual(response.status_code, 200)
+
